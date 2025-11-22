c9d8dcf273bc931d5e7b3d45abf5bce22ae219db
Swervin’ Curvin <cmiller9851@gmail.com>
2025-11-22 00:49:24 -0500

    Remove duplicate demo/love file

--- demo/love/love.py
+++
@@ -1,8 +0,0 @@
-def love_equation(intimacy, commitment, passion):
-return (intimacy + commitment + passion) / 3
-if name == "main":
-intimacy = 8
-commitment = 9
-passion = 7
-love_score = love_equation(intimacy, commitment, passion)
-print("Love Score:", love_score)
